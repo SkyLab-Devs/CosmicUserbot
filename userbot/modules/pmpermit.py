d4e03b0bcf102698ecded670b0a60dad61306b56
itsLuuke <67458252+AbOuLfOoOoOuF@users.noreply.github.com>
2021-11-04 09:28:38 +0200

    pmpermit: remove mono from the pm_msg

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -72,9 +72,9 @@ async def permitpm(event):
                         event.chat_id, from_user="me", search=UNAPPROVED_MSG
                     ):
                         await message.delete()
-                    await event.reply(f"`{UNAPPROVED_MSG}`")
+                    await event.reply(f"{UNAPPROVED_MSG}")
             else:
-                await event.reply(f"`{UNAPPROVED_MSG}`")
+                await event.reply(f"{UNAPPROVED_MSG}")
             LASTMSG.update({event.chat_id: event.text})
             if notifsoff:
                 await event.client.send_read_acknowledge(event.chat_id)
